59fd6cb16c91930cba0671e4d065cda63a8253b4
alvintriseptia <alvintryseptia@gmail.com>
2021-12-19 19:45:12 +0700

    cmon

--- models/upload.modal.js
+++ models/upload.modal.js
@@ -2,17 +2,12 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const uploadSchema = new Schema(
-	{
-		photo: {
-			type: String,
-			required: true,
-		},
+const uploadSchema = new Schema({
+	photo: {
+		type: String,
+		required: true,
 	},
-	{
-		collection: "img-uploader",
-	}
-);
+});
 
 const Upload = mongoose.model("Upload", uploadSchema);
 
